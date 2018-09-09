4cbd580299312ca02ec8701f2adfd1bd6d0f767a
Darius Tanz <darius1tanz>
2018-09-09 21:15:42 +0530

    First commit

---
+++ audio_text.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+
+# obtain path to "english.wav" in the same folder as this script
+from os import path
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "harvard.wav")
+
+# use the audio file as the audio source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
+
+# recognize speech using Sphinx
+try:
+    print("Output " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("could not understand audio")
+except sr.RequestError as e:
+    print("Output error; {0}".format(e))
+
+# For testing purpose recognize speech using Google Speech Recognition
+try:
+    print("Google Speech Recognition Result " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
